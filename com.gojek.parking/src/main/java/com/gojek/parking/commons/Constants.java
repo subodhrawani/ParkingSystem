1331842cc795afe35a93fc17ed080135799a69fb
srawani <srawani@juniper.net>
2019-03-12 23:01:12 +0530

    adding commons file

---
+++ com.gojek.parking/src/main/java/com/gojek/parking/commons/Constants.java
@@ -0,0 +1,13 @@
+package com.gojek.parking.commons;
+
+
+public class Constants {
+    public static final String CREATE_PARKING = "create_parking_lot";
+    public static final String ALLOCATE_PARKING = "park";
+    public static final String RELEASE_PARKING = "leave";
+    public static final String PARKING_STATUS = "status";
+    public static final String CARS_COLOUR_QUERY = "registration_numbers_for_cars_with_colour";
+    public static final String PARKING_COLOUR_QUERY = "slot_numbers_for_cars_with_colour";
+    public static final String PARKING_SLOT_QUERY = "slot_number_for_registration_number";
+    public static final String RESOURCE_DIRECTORY = "/";
+}
